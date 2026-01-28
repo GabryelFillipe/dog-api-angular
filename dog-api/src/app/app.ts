dd1ba64cdd62e00967865f842637d07ec2ab8360
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2026-01-28 09:50:59 -0300

    Ajudatando a configuração do projeto

--- dog-api/src/app/app.ts
+++ dog-api/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
+import { PrimeiroComponente } from './components/primeiro-componente/primeiro-componente';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimeiroComponente],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('dog-api');
+  protected readonly title = signal('gabryel');
 }
