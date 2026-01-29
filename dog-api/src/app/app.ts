e3559976b15a432c814285a1ea60fd0a98304c9a
Bre01cc <boliveiraassisreis@gmail.com>
2026-01-28 22:08:57 -0300

    feat:adicionando componente do main

--- dog-api/src/app/app.ts
+++ dog-api/src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet} from '@angular/router';
 import {Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
+import { Main } from './components/main/main';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer,Header],
+  imports: [RouterOutlet, Footer,Header,Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
